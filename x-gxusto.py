8faaa56680bfb7c8a2b0c47bef6940aac5a67814
zlong0825 <long@westtechtrans.org>
2017-02-22 19:58:34 +0800

    Update and rename x-gxusto to x-gxusto.py

--- x-gxusto.py
+++ x-gxusto.py
@@ -15,11 +15,17 @@ import sys,re,pyperclip
 #malbona stilo sed klara logiko, hehehe
 for i in range(6):
     teskto=re.sub('gx','ĝ',teksto)
+    teskto=re.sub('Gx','Ĝ',teksto)
     teksto=re.sub('cx','ĉ',teksto)
+    teksto=re.sub('Cx','Ĉ',teksto)
     teksto=re.sub('hx','ĥ',teksto)
+    teksto=re.sub('Hx','Ĥ',teksto)
     teksto=re.sub('jx','ĵ',teksto)
+    teksto=re.sub('Jx','Ĵ',teksto)
     teksto=re.sub('sx','ŝ',teksto)
+    teksto=re.sub('Sx','Ŝ',teksto)
     teksto=re.sub('ux','ŭ',teksto)
+    teksto=re.sub('Ux','ŭ',teksto)
 print(teksto)
 pyperclip.copy()
 
